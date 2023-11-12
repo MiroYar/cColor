b2a5336488d4fff94ea1b71e18dcd55d1a03b411
MiroYar <30885391+MiroYar@users.noreply.github.com>
2023-11-13 00:48:34 +0700

    Update test file

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 import { cColor } from '../index.js';
 
-console.log(cColor('test', 'FBM'));
-console.log(cColor('test', { F: 'M', B: 'B' }));
-console.log('\x1b[44;35mTEST\x1b[0m');
-console.log('\x1b[35;44mTEST\x1b[0m');
+console.log(cColor('Some text', 'FY'));
+console.log(cColor('Some text', 'BBM'));
+console.log(cColor('Some text', { F: 'Y', B: 'BM' }));
